b855c8f689604baabb51512b394314054ebbd720
jordanharris0 <jordanharris1833@yahoo.com>
2025-05-12 11:55:46 -0500

    finished

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+async function getImages(query) {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  try {
+    const response = await fetch(endpoint);
+    const json = await response.json();
+    const imageUrl = json.data[0].images.original.url;
+    console.log(imageUrl);
+  } catch (error) {
+    console.error("Error fetching data from Giphy API:", error);
+  }
+}
+
+getImages("cats");
+console.log(process.env.API_KEY);
